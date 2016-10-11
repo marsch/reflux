f713ca501cd9e53d94bf0f3cf5da3de977a99977
marsch <marsch.js@gmail.com>
2016-10-11 20:09:17 +0200

    bump

--- src/plugins/grid.app/context/AppContext.js
+++ src/plugins/grid.app/context/AppContext.js
@@ -1,8 +1,4 @@
 import React from 'react'
-import {contextual} from '@the-grid/multiverse-ui'
-import AppStore from './AppStore'
-
-const displayName = 'AppContext'
 
 // NOTE: probably not needed as a definition - could be the outcome of the store component or something
 class AppContext extends React.Component {
@@ -11,13 +7,4 @@ class AppContext extends React.Component {
   }
 }
 
-AppContext.PropTypes = {
-  children: React.PropTypes.any.isRequired
-}
-
-export default contextual.install(AppContext, 'app', () => {
-  const store = new AppStore({name: 'app', state: {}})
-  window.appStore = store
-
-  return store
-})
+export default AppContext
